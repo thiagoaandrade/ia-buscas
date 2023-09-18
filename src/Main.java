fa796440a3372d0ef059fbd5d0b5f10ef6853fad
thiagoaandrade <thiagoaandrade72@gmail.com>
2023-09-18 12:08:02 -0300

    Buscas adicionadas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import espacoDeEstados.*;
 import estrategiasDeBusca.heuristica.*;
+import estrategiasDeBusca.cega. *;
 
 public class Main {
 
@@ -10,6 +11,7 @@ public class Main {
 		//char[] cfgIni = {'2','4','3','7','1','6','5',' ','8'};
 		char[] cfgIni = {'2','3',' ','7','4','1','5','8','6'};
 		//char[] cfgIni = {'7','2','3','4',' ','1','5','8','6'}; // OutOfMemory
+		char[] cfgEnd = {'1','2','3','4','5','6','7',' ','8'};
 
 		Puzzle8 puzzleInicial = new Puzzle8();
 		puzzleInicial.setEstado(cfgIni);
@@ -17,11 +19,18 @@ public class Main {
 		puzzleInicial.setAvaliacao( puzzleInicial.heuristica(Puzzle8.TABULEIRO_ORGANIZADO) );
 			
 		Puzzle8 puzzleFinal = new Puzzle8();
-		puzzleFinal.setEstado( Puzzle8.TABULEIRO_ORGANIZADO );
+		puzzleFinal.setEstado( cfgEnd );
 		puzzleFinal.setCusto(0);
 		puzzleFinal.setAvaliacao(0);
-						
+				
+		//BuscaCega busca = new BuscaEmLargura();
+		//BuscaCega busca = new BuscaEmProfundidade();
+		//BuscaCega busca = new BuscaEmProfundidadeLimitada(null, null, 5);
+		
+		//BuscaInformada busca = new BestFirst();
 		BuscaInformada busca = new AStar();
+		//BuscaInformada busca = new HillClimbing();
+		
 		busca.setInicio(puzzleInicial);
 		busca.setObjetivo(puzzleFinal);
 		busca.buscar();
